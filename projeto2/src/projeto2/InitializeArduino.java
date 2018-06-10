72c2f961845891cdae5b5e2c9c3e70092a020db2
Alexandre Kajihara <alexandre.ykz@gmail.com>
2018-06-10 20:12:27 -0300

    Integrando o arduino com o java

--- projeto2/src/projeto2/InitializeArduino.java
+++ projeto2/src/projeto2/InitializeArduino.java
@@ -3,79 +3,82 @@ package projeto2;
 import javax.swing.*;
 
 public class InitializeArduino {
-	/* Tamanho da tela principal */
-	int height = 320;
-	int width = 140;
 
-	/* Tamanho da barra de progresso */
-	int progressBarWidth = 120;
-	int progressBarHeight = 25;
+    /* Tamanho da tela principal */
+    int height = 320;
+    int width = 140;
 
-	/* Coordenadas da barra de progresso */
-	int progressBarX = 95;
-	int progressBarY = 40;
+    /* Tamanho da barra de progresso */
+    int progressBarWidth = 120;
+    int progressBarHeight = 25;
 
-	/* Coordenadas dos labels */
-	int labelX = 65;
-	int labelY = 10;
+    /* Coordenadas da barra de progresso */
+    int progressBarX = 95;
+    int progressBarY = 40;
 
-	/* Tamanho dos bot�es */
-	int labelWidth = 200;
-	int labelHeight = 30;
-	
-	/* Minimo e m�ximo da barra de progresso */
-	int minimo = 0;
-	int maximo = 100;
-	
-	public InitializeArduino() throws InterruptedException {
-		/* Criando o JDialog */
-		JDialog dialog = new JDialog(new JFrame(), "Acenda e apague o Led", true);
+    /* Coordenadas dos labels */
+    int labelX = 65;
+    int labelY = 10;
 
-		JLabel labelInitialize = new JLabel("Tentando conex�o com arduino");
-		JProgressBar progressBar = new JProgressBar(minimo, maximo);
-		
-		labelInitialize.setVisible(true);
-		progressBar.setVisible(true);
-		
-		labelInitialize.setBounds(labelX, labelY, labelWidth, labelHeight);
-		progressBar.setBounds(progressBarX, progressBarY, progressBarWidth, progressBarHeight);
-		
-		dialog.add(labelInitialize);
-		dialog.add(progressBar);
-					
-		dialog.setLayout(null);
-		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		dialog.setSize(height, width);
-		dialog.setResizable(false);
-		dialog.setLocationRelativeTo(null);
-		
-		Thread t = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				dialog.setVisible(true);
-			}
-		});
-		t.start();
-		
-		for(int i = minimo; i <= maximo; i++){
-			if(i < 20){
-				labelInitialize.setText("Conectando com arduino...");
-                                AcessaArduino acessaArduino;
-                        } else if(i < 30)
-				labelInitialize.setText("Conectado!!!");
-			else
-				labelInitialize.setText("Definindo algumas configura��es...");
-			progressBar.setValue(i);
-			progressBar.setStringPainted(true);
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-	}
+    /* Tamanho dos bot�es */
+    int labelWidth = 200;
+    int labelHeight = 30;
+
+    /* Minimo e máximo da barra de progresso */
+    int minimo = 0;
+    int maximo = 100;
+
+    public InitializeArduino() throws InterruptedException {
+        AcessaArduino acessaArduino = new AcessaArduino();
+        /* Criando o JDialog */
+        JDialog dialog = new JDialog(new JFrame(), "Conexão com arduino", true);
+
+        JLabel labelInitialize = new JLabel("Tentando conexão com arduino");
+        JProgressBar progressBar = new JProgressBar(minimo, maximo);
+
+        labelInitialize.setVisible(true);
+        progressBar.setVisible(true);
+
+        labelInitialize.setBounds(labelX, labelY, labelWidth, labelHeight);
+        progressBar.setBounds(progressBarX, progressBarY, progressBarWidth, progressBarHeight);
+
+        dialog.add(labelInitialize);
+        dialog.add(progressBar);
+
+        dialog.setLayout(null);
+        dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        dialog.setSize(height, width);
+        dialog.setResizable(false);
+        dialog.setLocationRelativeTo(null);
+
+        Thread t = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                dialog.setVisible(true);
+            }
+        });
+        t.start();
+        
+        for (int i = minimo; i <= maximo; i++) {
+            if (i < 35) {
+                labelInitialize.setText("Procurando a porta...");
+            } else if (i < 65) {
+                labelInitialize.setText("Porta encontrada: " + acessaArduino.getPortaSelecionada());
+            } else {
+                labelInitialize.setText("Terminando contato com arduino...");
+            }
+            progressBar.setValue(i);
+            progressBar.setStringPainted(true);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        dialog.dispose();
+        MenuMain menuMain = new MenuMain(acessaArduino.getPortaSelecionada());
+    }
 }
--- projeto2/src/projeto2/MenuMain.java
+++ projeto2/src/projeto2/MenuMain.java
@@ -4,163 +4,164 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MenuMain {
-	/* Tamanho da tela de controle */
-	int height = 300;
-	int width = 360;
 
-	/* Tamanho dos bot�es */
-	int buttonWidth = 100;
-	int buttonHeight = 50;
+    /* Tamanho da tela de controle */
+    int height = 300;
+    int width = 360;
 
-	/* Coordenadas dos bot�es */
-	int buttonX = 95;
-	int buttonY = 40;
+    /* Tamanho dos bot�es */
+    int buttonWidth = 100;
+    int buttonHeight = 50;
 
-	/* Entre um bot�o e outro adiciona */
-	int distanceOfButton = 52;
-	
-	/* Coordenadas dos labels */
-	int labelX = 75;
-	int labelY = 10;
-	
-	/* Tamanho dos bot�es */
-	int labelWidth = 150;
-	int labelHeight = 30;
+    /* Coordenadas dos bot�es */
+    int buttonX = 95;
+    int buttonY = 40;
 
-	public MenuMain() {
-		/* Criando o JDialog */
-		JDialog dialog = new JDialog(new JFrame(), "Project car v2", true);
+    /* Entre um bot�o e outro adiciona */
+    int distanceOfButton = 52;
 
-		/* Criando as vari�veis da barra de menu */
-		JMenuBar barMenu = new JMenuBar();
-		JMenu helpMenu = new JMenu("Ajuda");
-		JMenuItem duvidasMenuItem = new JMenuItem("Tire suas d�vidas");
-		helpMenu.add(duvidasMenuItem);
-		barMenu.add(helpMenu);
+    /* Coordenadas dos labels */
+    int labelX = 75;
+    int labelY = 10;
 
-		/* Criando as vari�veis da label e dos bot�es */
-		JLabel labelMenu = new JLabel("Escolha uma das op��es: ");
-		JButton controleButton = new JButton("Controle");
-		JButton modoButton = new JButton("Modo");
-		JButton ledButton = new JButton("Led");
-		JButton contatoButton = new JButton("Contato");
-		JButton sairButton = new JButton("Sair");
+    /* Tamanho dos bot�es */
+    int labelWidth = 150;
+    int labelHeight = 30;
 
-		/* Tornando v�sivel a label e os bot�es */
-		labelMenu.setVisible(true);
-		controleButton.setVisible(true);
-		modoButton.setVisible(true);
-		ledButton.setVisible(true);
-		contatoButton.setVisible(true);
-		sairButton.setVisible(true);
-		
-		/* Definindo as coordenadas da label e os bot�es */
-		labelMenu.setBounds(labelX, labelY, labelWidth, labelHeight);
-		controleButton.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
-		modoButton.setBounds(buttonX, buttonY + distanceOfButton, buttonWidth, buttonHeight);
-		ledButton.setBounds(buttonX, buttonY + (2 * distanceOfButton), buttonWidth, buttonHeight);
-		contatoButton.setBounds(buttonX, buttonY + (3 * distanceOfButton), buttonWidth, buttonHeight);
-		sairButton.setBounds(buttonX, buttonY + (4 * distanceOfButton), buttonWidth, buttonHeight);
+    public MenuMain(String port) {
+        /* Criando o JDialog */
+        JDialog dialog = new JDialog(new JFrame(), "Project car v2", true);
 
-		/* Adicionando bot�es no panel */
-		dialog.add(labelMenu);
-		dialog.add(controleButton);
-		dialog.add(modoButton);
-		dialog.add(ledButton);
-		dialog.add(contatoButton);
-		dialog.add(sairButton);
-		
-		/* Chamando os listener dos bot�es */
-		controleButtonListener(controleButton);
-		modoButtonListener(modoButton);
-		ledButtonListener(ledButton);
-		contatoButtonListener(contatoButton);
-		sairButtonListener(sairButton);
-		helpMenuItemListener(duvidasMenuItem);
+        /* Criando as variáveis da barra de menu */
+        JMenuBar barMenu = new JMenuBar();
+        JMenu helpMenu = new JMenu("Ajuda");
+        JMenuItem duvidasMenuItem = new JMenuItem("Tire suas dúvidas");
+        helpMenu.add(duvidasMenuItem);
+        barMenu.add(helpMenu);
 
-		/* Adicionando tamanho, posi��o, fechar e ficar vis�vel */
-		dialog.setJMenuBar(barMenu);
-		dialog.setLayout(null);
-		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		dialog.setSize(height, width);
-		dialog.setResizable(false);
-		dialog.setLocationRelativeTo(null);
-		dialog.setVisible(true);
+        /* Criando as variáveis da label e dos botões */
+        JLabel labelMenu = new JLabel("Escolha uma das opções: ");
+        JButton controleButton = new JButton("Controle");
+        JButton modoButton = new JButton("Modo");
+        JButton ledButton = new JButton("Led");
+        JButton contatoButton = new JButton("Contato");
+        JButton sairButton = new JButton("Sair");
 
-	}
+        /* Tornando v�sivel a label e os botões */
+        labelMenu.setVisible(true);
+        controleButton.setVisible(true);
+        modoButton.setVisible(true);
+        ledButton.setVisible(true);
+        contatoButton.setVisible(true);
+        sairButton.setVisible(true);
 
-	void controleButtonListener(JButton controleButton) {
-		controleButton.addActionListener(new ActionListener() {
+        /* Definindo as coordenadas da label e os botões */
+        labelMenu.setBounds(labelX, labelY, labelWidth, labelHeight);
+        controleButton.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
+        modoButton.setBounds(buttonX, buttonY + distanceOfButton, buttonWidth, buttonHeight);
+        ledButton.setBounds(buttonX, buttonY + (2 * distanceOfButton), buttonWidth, buttonHeight);
+        contatoButton.setBounds(buttonX, buttonY + (3 * distanceOfButton), buttonWidth, buttonHeight);
+        sairButton.setBounds(buttonX, buttonY + (4 * distanceOfButton), buttonWidth, buttonHeight);
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new MenuControle();
-			}
+        /* Adicionando bot�es no panel */
+        dialog.add(labelMenu);
+        dialog.add(controleButton);
+        dialog.add(modoButton);
+        dialog.add(ledButton);
+        dialog.add(contatoButton);
+        dialog.add(sairButton);
 
-		});
-	}
+        /* Chamando os listener dos bot�es */
+        controleButtonListener(controleButton);
+        modoButtonListener(modoButton);
+        ledButtonListener(ledButton);
+        contatoButtonListener(contatoButton);
+        sairButtonListener(sairButton);
+        helpMenuItemListener(duvidasMenuItem);
 
-	void modoButtonListener(JButton modoButton) {
-		modoButton.addActionListener(new ActionListener() {
+        /* Adicionando tamanho, posi��o, fechar e ficar vis�vel */
+        dialog.setJMenuBar(barMenu);
+        dialog.setLayout(null);
+        dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        dialog.setSize(height, width);
+        dialog.setResizable(false);
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new MenuModo();
-			}
+    }
 
-		});
-	}
+    void controleButtonListener(JButton controleButton) {
+        controleButton.addActionListener(new ActionListener() {
 
-	void ledButtonListener(JButton ledButton) {
-		ledButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MenuControle();
+            }
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new MenuLed();
-			}
+        });
+    }
 
-		});
-	}
+    void modoButtonListener(JButton modoButton) {
+        modoButton.addActionListener(new ActionListener() {
 
-	void contatoButtonListener(JButton contatoButton) {
-		contatoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MenuModo();
+            }
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String message = "� Caso tenha d�vidas sobre o funcionamento mande um e-mail para: kikikiu@supermegacarrinho.com, com assunto: D�vidas Super Mega Carrinho;\n"
-						+ "� Caso tenha algum erro para reportar mande um e-mail para: tchaubrigado@supermegacarrinho.com, com assunto: Reportar Super Mega Carrinho;\n"
-						+ "� Se n�o ligue para o telefone: (66) 6666-6666.\n";
-				JOptionPane.showMessageDialog(null, message);
-			}
+        });
+    }
 
-		});
-	}
+    void ledButtonListener(JButton ledButton) {
+        ledButton.addActionListener(new ActionListener() {
 
-	void sairButtonListener(JButton sairButton) {
-		sairButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MenuLed();
+            }
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(1);
-			}
+        });
+    }
 
-		});
-	}
+    void contatoButtonListener(JButton contatoButton) {
+        contatoButton.addActionListener(new ActionListener() {
 
-	void helpMenuItemListener(JMenuItem helpMenuItem) {
-		helpMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "• Caso tenha dúvidas sobre o funcionamento mande um e-mail para: kikikiu@supermegacarrinho.com, com assunto: Dúvidas Super Mega Carrinho;\n"
+                        + "• Caso tenha algum erro para reportar mande um e-mail para: tchaubrigado@supermegacarrinho.com, com assunto: Reportar Super Mega Carrinho;\n"
+                        + "• Se não ligue para o telefone: (66) 6666-6666.\n";
+                JOptionPane.showMessageDialog(null, message);
+            }
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String message = "� Se voc� deseja pilotar o super mega carrinho clique no bot�o Controle;\n"
-						+ "� Se voc� deseja selecionar o modo de condu��o clique no bot�o Modo;\n"
-						+ "� Se voc� deseja acender os led do super mega carrinho clique no bot�o Led;\n"
-						+ "� Se voc� deseja entrar em contato conosco clique no bot�o Contato;\n"
-						+ "� Se voc� deseja sair clique no bot�o Sair.\n";
-				JOptionPane.showMessageDialog(null, message);
-			}
+        });
+    }
 
-		});
-	}
+    void sairButtonListener(JButton sairButton) {
+        sairButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(1);
+            }
+
+        });
+    }
+
+    void helpMenuItemListener(JMenuItem helpMenuItem) {
+        helpMenuItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "� Se voc� deseja pilotar o super mega carrinho clique no bot�o Controle;\n"
+                        + "� Se voc� deseja selecionar o modo de condu��o clique no bot�o Modo;\n"
+                        + "� Se voc� deseja acender os led do super mega carrinho clique no bot�o Led;\n"
+                        + "� Se voc� deseja entrar em contato conosco clique no bot�o Contato;\n"
+                        + "� Se voc� deseja sair clique no bot�o Sair.\n";
+                JOptionPane.showMessageDialog(null, message);
+            }
+
+        });
+    }
 
 }
