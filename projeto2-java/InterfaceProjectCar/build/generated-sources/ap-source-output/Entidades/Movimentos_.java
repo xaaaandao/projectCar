a8765008b7fe6f5a9cbec692f175c2b8feadbc09
ClodoaldoBasaglia <clodoaldofonseca92@gmail.com>
2018-06-25 13:15:36 -0300

    Alteração na conexão com o banco está dando problema

---
+++ projeto2-java/InterfaceProjectCar/build/generated-sources/ap-source-output/Entidades/Movimentos_.java
@@ -0,0 +1,16 @@
+package Entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Movimentos.class)
+public abstract class Movimentos_ {
+
+	public static volatile SingularAttribute<Movimentos, Integer> idmovimentos;
+	public static volatile SingularAttribute<Movimentos, String> direcao;
+	public static volatile SingularAttribute<Movimentos, String> tempo;
+
+}
+
